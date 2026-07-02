e4770c433d53a57d95eff625465a71bb32fa77f3
papaL3xa <john.scriven03@gmail.com>
2026-07-02 10:56:52 +0700

    deploy: include dist/, bersihkan kredensial searchlaporan.js, pencarian redirect ke Cek Transaksi

--- src/hadir.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './hadirApp.jsx';
-// App component is rendered inside hadirApp.jsx
--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './indexApp.jsx';
-// App component is rendered inside indexApp.jsx
