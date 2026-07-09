da4035a56fb7c0637b63a384deba4b57e66bde58
papaL3xa <john.scriven03@gmail.com>
2026-07-09 21:02:49 +0700

    fix: support custom domain easn.my.id

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/pisgahbisdac/',
   plugins: [react()],
   build: {
     rollupOptions: {
